eec467263295aaefd17d7a39307ff7c4ab769456
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-09-17 19:23:07 +0600

    delete contact by id

--- index.js
+++ index.js
@@ -153,6 +153,14 @@ async function run() {
         })
 
 
+        app.delete("/contacts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await contactCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
 
 
 
